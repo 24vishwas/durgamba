66ce626286dd3b61974b86584528f7f8ee3fd70a
Vishwas JS <vishwasjs2806@gmail.com>
2024-08-22 17:11:14 +0530

    first commit

--- index.js
+++ index.js
@@ -17,42 +17,42 @@ function checkOrientation() {
 }
 
 // testimonial
-let slideIndex = 0;
-const slides = document.querySelectorAll('.carousel-item');
-const dots = document.querySelectorAll('.dot');
+// let slideIndex = 0;
+// const slides = document.querySelectorAll('.carousel-item');
+// const dots = document.querySelectorAll('.dot');
 
-function showSlides(n) {
-  if (n >= slides.length) {
-    slideIndex = 0;
-  } else if (n < 0) {
-    slideIndex = slides.length - 1;
-  } else {
-    slideIndex = n;
-  }
+// function showSlides(n) {
+//   if (n >= slides.length) {
+//     slideIndex = 0;
+//   } else if (n < 0) {
+//     slideIndex = slides.length - 1;
+//   } else {
+//     slideIndex = n;
+//   }
 
-const offset = slideIndex * 100   ;
-  slides.forEach(slide => {
-    slide.style.transform = `translateX(-${offset}%)`;
-  });
+// const offset = slideIndex * 100   ;
+//   slides.forEach(slide => {
+//     slide.style.transform = `translateX(-${offset}%)`;
+//   });
 
-  dots.forEach(dot => dot.classList.remove('active'));
-  dots[slideIndex].classList.add('active');
-}
+//   dots.forEach(dot => dot.classList.remove('active'));
+//   dots[slideIndex].classList.add('active');
+// }
 
-function nextSlide() {
-  showSlides(slideIndex + 1);
-}
+// function nextSlide() {
+//   showSlides(slideIndex + 1);
+// }
 
-function prevSlide() {
-  showSlides(slideIndex - 1);
-}
+// function prevSlide() {
+//   showSlides(slideIndex - 1);
+// }
 
-function currentSlide(n) {
-  showSlides(n - 1);
-}
+// function currentSlide(n) {
+//   showSlides(n - 1);
+// }
 
-// Initial display
-showSlides(slideIndex);
+// // Initial display
+// showSlides(slideIndex);
 
 
 // services
